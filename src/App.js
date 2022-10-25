1d341603dcc3a36786b4c57bbaeafc4d1db987e9
Jesús Portillo Villa <jesus.portillo@sofka.com.co>
2022-10-24 23:32:02 -0500

    fetching pokemon list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import { useState, useEffect } from "react";
+
+const API = "https://pokeapi.co/api/v2/pokemon"
 
 function App() {
+  const [pokemonList, setpokemonList] = useState([])
+
+  useEffect(() => {
+    const pokeArray = []
+    fetch(`${API}?limit=50`)
+    .then(response => response.json())
+    .then(data => {
+        data.results.forEach(pokemon => {
+            fetch(pokemon.url)
+            .then(response => response.json())
+            .then(pokemonData => pokeArray.push(pokemonData));
+            setpokemonList(pokeArray)
+        });
+    })
+  },[])
+  
+  console.log(pokemonList)
   return (
     <div className="App">
-      Hola, Mundo!
+      Hola
     </div>
   );
 }
